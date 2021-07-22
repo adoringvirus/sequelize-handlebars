962e84087fbaa0bc61f8d9ac707f80497f7b466e
Wander95 <wanders1995@hotmail.com>
2021-07-22 14:22:20 -0400

    ✨ Add model
    -------> Movie model
    -------> User model
    -------> Landamark model

---
+++ src/models/movie.model.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequelize');
+const { MOVIES } = require('../config/database.tables');
+const sequelize = require('../database').bootstrap();
+
+const Movie = sequelize.define(MOVIES,{
+  movies_title: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+  movies_rating: {
+    type: Sequelize.INTEGER,
+  },
+  movies_description: {
+    type: Sequelize.STRING
+  },
+  movies_release_date: {
+    type: Sequelize.DATE
+  },
+  movies_url: {
+    type: Sequelize.STRING
+  },
+  movies_thumbnail: {
+    type: Sequelize.STRING
+  },
+  movies_created_at: {
+    type: Sequelize.DATE
+  },
+  movies_updated_at: {
+    type: Sequelize.DATE
+  },
+  movies_created_by: {
+    type: Sequelize.UUID
+  },
+  movies_updated_by: {
+    type: Sequelize.UUID
+  }
+},{ 
+  defaultScope: {
+    attributes: { 
+      exclude: ['createdAt','updatedAt'] 
+    }
+  },
+  timestamps:false
+})
+
+module.exports = Movie;
