43ea4829ea035e05297d33c20f547cb3eaa8b170
Wander95 <wanders1995@hotmail.com>
2021-07-22 16:13:55 -0400

    ✨ Add model
    ----> user-roles , user-status

---
+++ src/models/user-status.model.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const { USER_STATUS } = require('../config/database.tables');
+const sequelize = require('../database/database').bootstrap();
+
+const UserStatus = sequelize.define(USER_STATUS,{
+  user_status_name: {
+    type: Sequelize.STRING
+  },
+  user_status_description: {
+    allowNull: false,
+    type: Sequelize.STRING
+  }
+},{
+  defaultScope: {
+    attributes: { 
+      exclude: ['createdAt','updatedAt'] 
+    },
+  },
+  timestamps: false,
+  freezeTableName: true
+})
+
+module.exports = UserStatus
