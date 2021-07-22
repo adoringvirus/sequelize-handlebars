97549a06e615b95e42ad4d9a03ca438ddb04db24
Wander95 <wanders1995@hotmail.com>
2021-07-22 14:24:08 -0400

    ♻️ Add conditional and env variables

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,8 +1,8 @@
 
 module.exports = {
   POSTGRES_USER:process.env.POSTGRES_USER || 'postgres', 
-  POSTGRES_DB:process.env.POSTGRES_DB || 'postgres',
+  POSTGRES_DB:process.env.POSTGRES_DB || 'directory',
   POSTGRES_PASSWORD:process.env.POSTGRES_PASSWORD || '123456789', 
-  POSTGRES_HOST:process.env.POSTGRES_HOST || "172.17.0.2",
+  POSTGRES_HOST:process.env.POSTGRES_HOST || "localhost",
   dialect: 'postgres'
 }
