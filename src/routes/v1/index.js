cc2eda43fc90dc6bb6d3a0783e4d207a773e974e
Wander95 <wanders1995@hotmail.com>
2021-07-22 16:14:50 -0400

    ✨ Add routes / Base CRUD
    ----> user-roles, user-status, movies

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const MoviesRouter = require('./movie.routes');
+const UserRolesRouter = require('./user-roles.routes');
+const UserStatusRouter = require('./user-status.routes');
 const UserRouter = require('./user.routes');
 
 const V1RootRouter = Router();
@@ -11,7 +13,9 @@ V1RootRouter.get('/',(req,res)=>{
 })
 
 V1RootRouter.use(UserRouter);
-V1RootRouter.use(MoviesRouter)
+V1RootRouter.use(MoviesRouter);
+V1RootRouter.use(UserStatusRouter);
+V1RootRouter.use(UserRolesRouter)
 
 
 module.exports = V1RootRouter
