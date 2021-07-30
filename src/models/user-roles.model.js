215418f79290da37f1490e9c695ef6d70df4ade5
Wander95 <wanders1995@hotmail.com>
2021-07-30 15:19:09 -0400

    ✨ Add user relation

--- src/models/user-roles.model.js
+++ src/models/user-roles.model.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const { USER_ROLES } = require('../config/database.tables');
+const UserModel = require('./user.model');
 const sequelize = require('../database/database').bootstrap();
 
 const UserRolesModel = sequelize.define(USER_ROLES,{
@@ -18,5 +19,29 @@ const UserRolesModel = sequelize.define(USER_ROLES,{
   timestamps: false,
   freezeTableName: true
 })
+
+
+UserModel.belongsTo(UserRolesModel,{
+  foreignKey: {
+    name:'users_roles_id',
+    type: Sequelize.DataTypes.UUID
+  }
+})
+
+UserRolesModel.hasMany(UserModel,{
+  foreignKey: {
+    name:'users_roles_id',
+    type: Sequelize.DataTypes.UUID
+  }
+})
+
+// 
+
+// UserRolesModel.hasMany(UserModel,{
+//   foreignKey: {
+//     name:'users_roles_id',
+//     type: Sequelize.DataTypes.UUID
+//   }
+// })
 
 module.exports = UserRolesModel;
