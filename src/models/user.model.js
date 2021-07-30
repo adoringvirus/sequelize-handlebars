969ecb17dca650729d81450f0e69279792864534
Wander95 <wanders1995@hotmail.com>
2021-07-30 15:21:37 -0400

    ✨ Add relations
    -------> Comments
    -------> Movies
    -------> User Features
    -------> user Status

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,13 @@
 const Sequelize = require('sequelize');
 const { USERS } = require('../config/database.tables');
+const CommentsModel = require('./comments.model');
+const MovieModel = require('./movie.model');
+const MovieCategoryRelationModel = require('./relations/movies-category.model');
+const UsersFeaturesRelationModel = require('./relations/user-features-users.model');
+const UserMovieCommentsRelation = require('./relations/users-movies-comments.model');
+const UserFeaturesModel = require('./user-features.model');
+// const UserRolesModel = require('./user-roles.model');
+const UserStatusModel = require('./user-status.model');
 const sequelize = require('../database/database').bootstrap();
 
 const UserModel = sequelize.define(USERS,{
@@ -35,10 +43,10 @@ const UserModel = sequelize.define(USERS,{
     type: Sequelize.STRING
   },
   users_roles_id: {
-    type: Sequelize.DATE
+    type: Sequelize.UUID,
   },
   users_status_id: {
-    type: Sequelize.DATE,
+    type: Sequelize.UUID,
   },
   users_created_at: {
     type: Sequelize.DATE
@@ -63,9 +71,76 @@ const UserModel = sequelize.define(USERS,{
     attributes: { 
       exclude: ['createdAt','updatedAt'] 
     }
+  }, 
+  hooks:{
+    // beforeCreate
+  },
+  instanceMethods:{
+    // validPassword
   },
   timestamps: false,
   freezeTableName: true
+})
+
+// UserRolesModel.hasMany(UserModel,{
+//   foreignKey: 'users_id',
+// })
+
+
+UserModel.belongsToMany(MovieModel,{
+  through: UserMovieCommentsRelation,
+  foreignKey:'users_id'
+})
+
+MovieModel.belongsToMany(UserModel,{
+  through: UserMovieCommentsRelation,
+  foreignKey:'movies_id'
+})
+
+
+// UserModel.belongsTo(UserRolesModel,{
+//   foreignKey: 'users_id'
+// })
+
+// UserRolesModel.hasMany(UserModel,{
+//   foreignKey: 'users_id',
+// })
+
+UserModel.belongsToMany(CommentsModel,{
+  through: UserMovieCommentsRelation,
+  foreignKey: 'users_id',
+})
+
+CommentsModel.belongsToMany(UserModel,{
+  through: UserMovieCommentsRelation,
+  foreignKey: 'comments_id',
+})
+
+UserModel.belongsToMany(UserFeaturesModel,{
+  through: UsersFeaturesRelationModel,
+  foreignKey:'users_id',
+})
+
+UserFeaturesModel.belongsToMany(UserModel,{
+  through: UsersFeaturesRelationModel,
+  foreignKey: 'user_features_id',
+  onDelete:'CASCADE'
+})
+
+
+
+UserModel.belongsTo(UserStatusModel,{
+  foreignKey: {
+    name:'users_status_id',
+    type: Sequelize.DataTypes.UUID
+  }
+});
+
+UserStatusModel.hasMany(UserModel,{
+  foreignKey: {
+    name:'users_status_id',
+    type: Sequelize.DataTypes.UUID
+  }
 })
 
 module.exports = UserModel
