11f20631ffe865468fd91afdf2bf80d29f3e3d9a
Wander95 <wanders1995@hotmail.com>
2021-07-30 14:28:34 -0400

    ✨ Add relations with movies

--- src/models/comments.model.js
+++ src/models/comments.model.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 const { COMMENTS } = require('../config/database.tables');
+const MovieModel = require('./movie.model');
+const UserMovieCommentsRelation = require('./relations/users-movies-comments.model');
 const sequelize = require('../database/database').bootstrap();
 
 const CommentsModel = sequelize.define(COMMENTS,{
@@ -28,7 +30,23 @@ const CommentsModel = sequelize.define(COMMENTS,{
     },
   },
   timestamps: false,
-  freezeTableName: true
+  freezeTableName: true,
+  underscored: true,
+})
+
+CommentsModel.belongsToMany(MovieModel,{
+  through: UserMovieCommentsRelation,
+  foreignKey: 'comments_id',
+})
+
+// UserModel.belongsToMany(CommentsModel,{
+//   through: UserMovieCommentsRelation,
+//   foreignKey: 'comments_id',
+// })
+
+MovieModel.belongsToMany(CommentsModel,{
+  through: UserMovieCommentsRelation,
+  foreignKey: 'movies_id',
 })
 
 module.exports = CommentsModel;
