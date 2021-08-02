bfb38f5dbefd260d2211f8310d1fa87611735c08
Wander95 <wanders1995@hotmail.com>
2021-08-02 12:43:16 -0400

    ✏️ Change database

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   POSTGRES_USER:process.env.POSTGRES_USER || 'postgres', 
-  POSTGRES_DB:process.env.POSTGRES_DB || 'directory',
+  POSTGRES_DB:process.env.POSTGRES_DB || 'postgres',
   POSTGRES_PASSWORD:process.env.POSTGRES_PASSWORD || '123456789', 
   POSTGRES_HOST:process.env.POSTGRES_HOST || "localhost",
   dialect: 'postgres'
