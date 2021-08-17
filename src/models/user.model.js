8e3f94d6bba73607b04075147d994d7f8f4b6025
Wander95 <wanders1995@hotmail.com>
2021-08-17 01:44:09 -0400

    ✨ hash password before saving it

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,12 +2,13 @@ const Sequelize = require('sequelize');
 const { USERS } = require('../config/database.tables');
 const CommentsModel = require('./comments.model');
 const LikeModel = require('./like.model');
-// const MovieModel = require('./movie.model');
 const UsersFeaturesRelationModel = require('./relations/user-features-users.model');
 const ReviewsModel = require('./reviews.model');
 const UserFeaturesModel = require('./user-features.model');
 const UserStatusModel = require('./user-status.model');
 const sequelize = require('../database/database').bootstrap();
+const bcrypt = require('bcrypt');
+
 
 const UserModel = sequelize.define(USERS,{
   user_username: {
@@ -26,7 +27,7 @@ const UserModel = sequelize.define(USERS,{
   },
   user_email:{
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
   user_password: {
     allowNull: false,
@@ -66,19 +67,22 @@ const UserModel = sequelize.define(USERS,{
       return this.user_first_name + ' ' + this.user_last_name;
     }
   },
-  defaultScope: {
-    attributes: { 
-      exclude: ['createdAt','updatedAt'] 
-    }
-  }, 
   hooks:{
-    // beforeCreate
+    beforeCreate : async (user,options)=>{
+      try {
+        const hashedPassword = await bcrypt.hash(user.user_password,10);
+        user.user_password = hashedPassword;
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
-  instanceMethods:{
-    // validPassword
-  },
+  // instanceMethods:{
+  //   // validPassword
+  // },
   timestamps: false,
-  freezeTableName: true
+  freezeTableName: true,
+  underscored: true
 })
 
 
