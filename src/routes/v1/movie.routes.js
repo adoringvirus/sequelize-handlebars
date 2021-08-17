8f57c819e420a7c4b2d6ece85ca38a1815d03fa4
Wander95 <wanders1995@hotmail.com>
2021-08-17 01:51:02 -0400

    ✨ Add route protection

--- src/routes/v1/movie.routes.js
+++ src/routes/v1/movie.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
+const { assignFilmakingMemberToMovie, unsignedFilmakingMemberFromMovie } = require('../../controller/filmaking-member.controller');
 const { getAllMovies, createMovie, updateMovie, getOneMovie, deleteMovie } = require('../../controller/movie.controller');
+const { isSuperAdmin } = require('../../middlewares/auth.middleware');
 
 
 const MoviesRouter = Router();
@@ -7,8 +9,19 @@ const MOVIE_ROUTE_PREFIX = '/movies'
 
 MoviesRouter.get(`${MOVIE_ROUTE_PREFIX}`,getAllMovies);
 MoviesRouter.get(`${MOVIE_ROUTE_PREFIX}/:id`,getOneMovie);
-MoviesRouter.post(`${MOVIE_ROUTE_PREFIX}`,createMovie);
-MoviesRouter.put(`${MOVIE_ROUTE_PREFIX}/:id`, updateMovie);
-MoviesRouter.delete(`${MOVIE_ROUTE_PREFIX}/:id`,deleteMovie);
+MoviesRouter.post(`${MOVIE_ROUTE_PREFIX}`,isSuperAdmin,createMovie);
+MoviesRouter.put(`${MOVIE_ROUTE_PREFIX}/:id`,isSuperAdmin, updateMovie);
+MoviesRouter.delete(`${MOVIE_ROUTE_PREFIX}/:id`,isSuperAdmin,deleteMovie);
+
+MoviesRouter.post(
+  `${MOVIE_ROUTE_PREFIX}/:movieId/filmaking-members/:filmakingMemberId`,
+  isSuperAdmin,
+  assignFilmakingMemberToMovie
+);
+MoviesRouter.delete(`
+  ${MOVIE_ROUTE_PREFIX}/:movieId/filmaking-members/:filmakingMemberId`,
+  isSuperAdmin,
+  unsignedFilmakingMemberFromMovie
+);
 
 module.exports = MoviesRouter;
--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const { getAllUsers, createUser, getOneUser, updateUser, deleteUser } = require('../../controller/user.controller');
+const { isSuperAdmin } = require('../../middlewares/auth.middleware');
 
 const UserRouter = Router();
 const USER_ROUTE_PREFIX = '/users';
 
-UserRouter.get(`${USER_ROUTE_PREFIX}`,getAllUsers);
-UserRouter.get(`${USER_ROUTE_PREFIX}/:id`,getOneUser);
-UserRouter.post(`${USER_ROUTE_PREFIX}`,createUser);
-UserRouter.put(`${USER_ROUTE_PREFIX}/:id`,updateUser);
-UserRouter.delete(`${USER_ROUTE_PREFIX}/:id`,deleteUser);
+UserRouter.get(`${USER_ROUTE_PREFIX}`,isSuperAdmin,getAllUsers);
+UserRouter.get(`${USER_ROUTE_PREFIX}/:id`,isSuperAdmin,getOneUser);
+UserRouter.post(`${USER_ROUTE_PREFIX}`,isSuperAdmin,createUser);
+UserRouter.put(`${USER_ROUTE_PREFIX}/:id`,isSuperAdmin,updateUser);
+UserRouter.delete(`${USER_ROUTE_PREFIX}/:id`,isSuperAdmin,deleteUser);
 
 module.exports = UserRouter;
