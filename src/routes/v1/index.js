5fb78f7a93dfdbb03ba9769b8904ab142624596c
Wander95 <wanders1995@hotmail.com>
2021-09-01 14:51:08 -0400

    ♻️ Direct import in use function

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,35 +1,19 @@
 const { Router } = require('express');
-const CategoryRouter = require('./category.routes');
-const CommentsRouter = require('./comments.routes');
-const FilmakingMembersRolesRouter = require('./filmaking-members-roles.routes');
-const FilmakingMembersRouter = require('./filmaking-members.routes');
-const LikeRoutes = require('./like.routes');
-const MoviesRouter = require('./movie.routes');
-const ReviewsRouter = require('./reviews.routes');
-const UserFeaturesRouter = require('./user-features.routes');
-const UserRolesRouter = require('./user-roles.routes');
-const UserStatusRouter = require('./user-status.routes');
-const UserRouter = require('./user.routes');
+const { isParamValid } = require('../../middlewares/validation.middleware');
 
 const V1RootRouter = Router();
 
-V1RootRouter.get('/',(req,res)=>{
-  res.json({
-    checking:'hellooo'
-  })
-})
-
-V1RootRouter.use(UserStatusRouter);
-V1RootRouter.use(UserRolesRouter);
-V1RootRouter.use(UserFeaturesRouter);
-V1RootRouter.use(CategoryRouter),
-V1RootRouter.use(ReviewsRouter);
-V1RootRouter.use(CommentsRouter);
-V1RootRouter.use(LikeRoutes);
-V1RootRouter.use(FilmakingMembersRouter);
-V1RootRouter.use(FilmakingMembersRolesRouter);
-V1RootRouter.use(UserRouter);
-V1RootRouter.use(MoviesRouter);
+V1RootRouter.use( require('./user-status.routes') );
+V1RootRouter.use( require('./user-roles.routes') );
+V1RootRouter.use( require('./user-features.routes') );
+V1RootRouter.use( require('./category.routes') ),
+V1RootRouter.use( require('./reviews.routes') );
+V1RootRouter.use( require('./comments.routes') );
+V1RootRouter.use( require('./like.routes') );
+V1RootRouter.use( require('./filmaking-members.routes') );
+V1RootRouter.use( require('./filmaking-members-roles.routes') );
+V1RootRouter.use( require('./user.routes') );
+V1RootRouter.use( require('./movie.routes') );
 
 
 module.exports = V1RootRouter
--- src/validation/MovieSchema.js
+++ src/validation/MovieSchema.js
@@ -1,15 +1,13 @@
 const Yup = require('yup');
 
-const stringMessage = 'Value must be string';
-const requiredMessage = 'Value must be provided'
 const MovieSchema = Yup.object({
-  movie_title: Yup.string(stringMessage).required(requiredMessage),
-  movie_rating: Yup.number(stringMessage).min(0).max(5).required(requiredMessage),
+  movie_title: Yup.string().required(),
+  movie_rating: Yup.number().min(0).max(5).required(),
   like_count: Yup.number("value must be a number"),
-  movie_description: Yup.string(stringMessage).required(requiredMessage),
-  movie_release_date:Yup.string(stringMessage).required(requiredMessage), 
-  movie_url: Yup.string(stringMessage).required(requiredMessage).url(),
-  movie_thumbnail: Yup.string(stringMessage).required(requiredMessage)
+  movie_description: Yup.string().required(),
+  movie_release_date:Yup.string().required(), 
+  movie_url: Yup.string().required().url(),
+  movie_thumbnail: Yup.string().required()
 })
 
 module.exports = MovieSchema;
