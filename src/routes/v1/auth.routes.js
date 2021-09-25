fe58175514f3188b42de3661372c893e9f2a84f8
Wander95 <wanders1995@hotmail.com>
2021-09-25 11:58:19 -0400

    ✨ Add harcoded jwt

--- src/routes/v1/auth.routes.js
+++ src/routes/v1/auth.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const { signIn, signUp, logout, verifyEmail } = require('../../controller/auth.controller');
 
@@ -11,6 +12,13 @@ AuthRouter.get('/auth/signin',passport.authenticate('local',{
 
 AuthRouter.post('/auth/signup',signUp);
 AuthRouter.get('/auth/logout',logout);
+AuthRouter.get('/authenticate',async (req,res)=>{
+
+  const singed = jwt.sign({user:'lui',id:2,email:'auth@gmail.com'},'SECRET');
+  return res.json({
+    jwt:singed
+  })
+})
 AuthRouter.get('/auth/verify-email/:tokenId',verifyEmail);
 // AuthRouter.get('/auth/reset-password');
 // AuthRouter.get('/auth/change-email');
