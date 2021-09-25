1431d71bdec5bb453869f570bf6d1419a0bfc351
Wander95 <wanders1995@hotmail.com>
2021-09-25 11:56:56 -0400

    ♻️ Nomalize responses
    -----> Remove check if user object exist

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,3 +1,4 @@
+const { RESPONSES } = require('../responses/response');
 
 
 module.exports = {
@@ -21,18 +22,80 @@ module.exports = {
     next 
   ){
     // * if there is no user 
-    if(!req.user){return res.status(401).json({message: 'You are not authorized'})}  
+    if( !req.isAuthenticated() ) { return RESPONSES.BAD_REQUEST(res,{
+      path: req.originalUrl,
+      code:401,
+      message: 'You are not logged in',
+      error: true,
+      data: null
+    })};
+
     req.user = req.session.passport.user;
 
     const {  user_status,user_role,user_features } = req.session.passport.user;
     
-    const userStatus = user_status.user_status_name || null;
+    const userStatus = user_status 
+    ? user_status.user_status_name : null;
+
     const userRole = user_role.user_role_name || null;
     const userFeature = user_features || null;
 
-    if( !req.isAuthenticated() ) { return res.status(401) };
-    if( userStatus !== 'active' ) { return res.json({message:'User is Inactive'}) };
-    if( userRole !== 'superadmin' ) { return res.json({message:'User is not super-admin'}) };
+
+
+    if( userStatus !== 'active' ) { return RESPONSES.BAD_REQUEST(res,{
+      path: req.originalUrl,
+      code:401,
+      message: 'User is not active',
+      error: true,
+      data: null
+    })};
+
+    if( userRole !== 'superadmin' ) { return RESPONSES.BAD_REQUEST(res,{
+      path: req.originalUrl,
+      code:401,
+      message: 'User is not superadmin',
+      error: true,
+      data: null
+    })};
+
+    next();
+  },
+  isLoggedIn(
+    /** @type {import('express').Request } */
+    req,
+    /** @type {import('express').Response } */
+    res,
+    /** @type {import('express').NextFunction } */
+    next 
+  ){
+    // * if there is no user 
+    if( !req.isAuthenticated() ) { return RESPONSES.BAD_REQUEST(res,{
+      path: req.originalUrl,
+      code:401,
+      message: 'You are not logged in',
+      error: true,
+      data: null
+    })};
+
+    req.user = req.session.passport.user;
+
+    const {  user_status,user_role,user_features } = req.session.passport.user;
+    
+    const userStatus = user_status 
+    ? user_status.user_status_name : null;
+
+    const userRole = user_role.user_role_name || null;
+    const userFeature = user_features || null;
+
+    if( userStatus !== 'active' ) { return RESPONSES.BAD_REQUEST(res,{
+      path: req.originalUrl,
+      code:401,
+      message: 'User is not active',
+      error: true,
+      data: null
+    })};
+
+    // if( userRole !== 'user' ) { return res.json({message:'Not a user'}) };
     
     next();
   }
