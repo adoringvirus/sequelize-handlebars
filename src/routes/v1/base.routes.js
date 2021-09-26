8c3d5bff0118df3aceb3be7d977214bd0b760be9
Wander95 <wanders1995@hotmail.com>
2021-09-26 14:58:55 -0400

    ✨ Add dynamic middelwares
    -----> habilitie to add as many middlewares as posible on each method

--- src/routes/v1/base.routes.js
+++ src/routes/v1/base.routes.js
@@ -1,61 +1,77 @@
 const { baseController } = require('../../controller/base.controller');
 const { isSuperAdmin } = require('../../middlewares/auth.middleware');
 const { isParamValid } = require('../../middlewares/validation.middleware');
+const { cloneDeep } = require('lodash');
+const { isValidBody } = require('../../middlewares/validation.middleware');
+const UserStatusSchema = require('../../validation/UserStatusSchema');
 
 const defaultOptions = {
-  isGetProtected: true,
-  isGetOneProtected: true,
-  isPostProtected: true,
-  isPutProtected: true,
-  isDeleteProtected: true
+  getOptions:{
+    isGetProtected: true,
+    getMiddlewares: []
+  },
+  getOneOptions:{
+    isGetOneProtected: true,
+  },
+  postOptions:{
+    isPostProtected: true,
+    postMiddlewares: []
+  },
+  putOptions:{
+    isPutProtected: true,
+    putMiddlewares: []
+  },
+  deleteOptions:{
+    isDeleteProtected: true,
+    deleteMiddlewares: []
+  }
 }
 
-exports.baseRoute = (router,model,modelName,routeOptions=defaultOptions)=>{
+exports.baseRoute = (router,model,modelName,routeOptions=defaultOptions,)=>{
   
 
   // * Combination of incoming routes and default ones to make it 
   // * easier and optional when passing object
   const combinedRouteOptions = {
-    ...defaultOptions,
-    ...routeOptions
+    ...cloneDeep(defaultOptions),
+    ...cloneDeep(routeOptions)
   }
 
-  
   const { 
     getAllModels,getOneModel,
     createModel,updateModel,deleteModel
   } = baseController(model,modelName);
-  
+  const { getOptions,getOneOptions,postOptions,putOptions,deleteOptions } = combinedRouteOptions
   // * get all models
-  if( combinedRouteOptions.isGetProtected ){
+  if( getOptions.isGetProtected ){
     router.get(`/${modelName}`,isSuperAdmin,getAllModels);
   }else{
     router.get(`/${modelName}`,getAllModels);
   }
 
   // * get one model
-  if( combinedRouteOptions.isGetOneProtected ){
+  if( getOneOptions.isGetOneProtected ){
     router.get(`/${modelName}/:id`,isParamValid,isSuperAdmin,getOneModel);
   }else{
     router.get(`/${modelName}/:id`,isParamValid,getOneModel);
   }
 
   // * create one model
-  if( combinedRouteOptions.isPostProtected ){
-    router.post(`/${modelName}`,isSuperAdmin,createModel);
+  if( postOptions.isPostProtected ){
+    router.post(`/${modelName}`,postOptions.postMiddlewares,isSuperAdmin,createModel);
   }else{
-    router.post(`/${modelName}`, createModel);
+    router.post(`/${modelName}`,postOptions.postMiddlewares, createModel);
   }
 
   // * update model
-  if( combinedRouteOptions.isPutProtected ){
-    router.put(`/${modelName}/:id`,isParamValid,isSuperAdmin, updateModel);
+  if( putOptions.isPutProtected ){
+    router.put(`/${modelName}/:id`,putOptions.putMiddlewares,isParamValid,isSuperAdmin, updateModel);
   }else{
-    router.put(`/${modelName}/:id`,isParamValid, updateModel);
+    router.put(`/${modelName}/:id`,putOptions.putMiddlewares,isParamValid, updateModel);
   }
 
   // * delete model
-  if( combinedRouteOptions.isDeleteProtected ){
+  if( deleteOptions.isDeleteProtected ){
     router.delete(`/${modelName}/:id`,isParamValid,isSuperAdmin,deleteModel);
   }else{
     router.delete(`/${modelName}/:id`,isParamValid, deleteModel);
