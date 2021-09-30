017e1d06c13ca5000ea913df6c3287a96c635b5f
Wander95 <wanders1995@hotmail.com>
2021-09-30 00:42:00 -0400

    -- No breaking changes --

--- src/redis/redis.store.js
+++ src/redis/redis.store.js
@@ -1,9 +1,15 @@
 const redis = require('redis');
 const redisClient = redis.createClient();
-
+const databaseConfig = require('../config/database.config');
 module.exports = (session) =>{
   const redisStore = require('connect-redis')(session);  
+  const pgSession = require('connect-pg-simple')(session);
 
+  // const postgresStore = new pgSession({
+  //   // pool:databaseConfig.pool,
+  //   conString: databaseConfig.PG_CONNECT_STRING,
+  //   tableName: 'session'
+  // })
   redisClient.on('error', (err) => {
     console.log('Redis error: ', err);
   });
